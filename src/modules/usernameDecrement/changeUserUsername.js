e02ce1e0da26ee71533fe24431a2aeb3c0853a98
Bojan Maksimovic <bojanm245@gmail.com>
2023-07-17 10:26:48 +0200

    Fix changeUserUsername bug

--- src/modules/usernameDecrement/changeUserUsername.js
+++ src/modules/usernameDecrement/changeUserUsername.js
@@ -1,25 +1,13 @@
 const { targetUserId, serverId } = require('../../../config.json');
 
 module.exports = async (username, client) => {
-    let server = null;
-    await client.guilds.fetch(serverId)
-        .then(async data => server = data)
-        .catch(error => handleError(error));
-
+    const server = await client.guilds.fetch(serverId);
     if (!server) return false;
     
-    let targerUser = null;
-    await server.members.fetch(targetUserId)
-        .then(data => targerUser = data)
-        .catch(error => handleError(error));
-
-    if (!targerUser) return false;
+    const targetUser = await server.members.fetch(targetUserId);
+    if (!targetUser) return false;
 
     targetUser.setNickname(username);
 
     return true;
 };
-
-function handleError(error) {
-    console.error(`An error occured: ${error}`);
-}
