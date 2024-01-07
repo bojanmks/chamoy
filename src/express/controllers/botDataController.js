73b3e0122da69ca862436e4ba98ddb6fe3ca31dc
Bojan Maksimovic <bojanm245@gmail.com>
2024-01-07 22:12:39 +0100

    Add presence to the botData response

--- src/express/controllers/botDataController.js
+++ src/express/controllers/botDataController.js
@@ -2,9 +2,16 @@ const get_data = {
     route: '/',
     method: 'get',
     callback : (req, res, next, client) => {
+        const botActivity = client.presence?.activities?.length
+            ? client.presence.activities[0]
+            : null;
+
         return {
             name: client.user.username,
-            avatar: client.user.displayAvatarURL()
+            avatar: client.user.displayAvatarURL(),
+            presence: botActivity
+                ? { name: botActivity.name, type: botActivity.type }
+                : null
         };
     }
 }
