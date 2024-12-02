64183056dacd8a3bffb22dfa88237bfc7d5a89b9
Bojan Maksimovic <bojanm245@gmail.com>
2024-12-02 21:33:32 +0100

    Delete the restart command and make the ping command available in all environments

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,17 +1,13 @@
 import useCommands from "@modules/commands/useCommands";
-import useEnvironments from "@modules/environments/useEnvironments";
 import useReplying from "@modules/messaging/useReplying";
 import { Client, CommandInteraction } from "discord.js";
 
 const { sendTextReply } = useReplying();
-const { DEVELOPMENT_ENVIRONMENT } = useEnvironments();
 const { BaseCommand } = useCommands();
 
 class PingCommand extends BaseCommand {
     name: string = 'ping';
     description?: string = '🏓';
-    
-    override environments?: string[] = [DEVELOPMENT_ENVIRONMENT];
 
     override hasEphemeralResponse?: boolean | undefined = true;
     
