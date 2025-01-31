438673528eae4892ad1bb52ee78add353bf7b73e
Bojan Maksimovic <bojanm245@gmail.com>
2025-01-31 23:56:56 +0100

    Set the auth cookie maxAge to 15 minutes

--- src/modules/auth/useDiscordAuth.ts
+++ src/modules/auth/useDiscordAuth.ts
@@ -30,8 +30,7 @@ const setupDiscordAuth = async (app: Express) => {
             cookie: {
                 httpOnly: true,
                 secure: !isDevelopment(),
-                //maxAge: 15 * 60 * 1000 // 15 minutes
-                maxAge: 5 * 1000 // 5 seconds
+                maxAge: 15 * 60 * 1000 // 15 minutes
             }
         })
     );
