6543a44e6215b228019fbf289c628689579cd1ca
Bojan Maksimovic <bojanm245@gmail.com>
2025-02-02 11:33:53 +0100

    Update Game interface

--- src/modules/games/models/Game.ts
+++ src/modules/games/models/Game.ts
@@ -1,6 +1,6 @@
+import { GameLink } from "./GameLink";
 import { IEntity } from "@database/models/IEntity";
 import { ISoftDeletableEntity } from "@database/models/ISoftDeletableEntity";
-import { GameLink } from "@prisma/client";
 
 export interface Game extends IEntity, ISoftDeletableEntity {
     name: string;
